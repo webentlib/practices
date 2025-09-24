720cff9265836bc6127939a55c082f5790c76543
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-09-24 12:03:26 +0300

    Yet another commit

--- patch_model_field_to_accept_group_param.py
+++ patch_model_field_to_accept_group_param.py
@@ -66,5 +66,62 @@ def __init__(
     self._error_messages = error_messages  # Store for deconstruction later
 
 
+from django import forms
+from django.utils.text import capfirst
+
+
+def formfield(self, form_class=None, choices_form_class=None, **kwargs):
+    """Return a django.forms.Field instance for this field."""
+    defaults = {
+        "required": not self.blank,
+        # "label": capfirst(self.verbose_name),
+        "label": self.verbose_name,
+        "help_text": self.help_text,
+    }
+    if self.has_default():
+        if callable(self.default):
+            defaults["initial"] = self.default
+            defaults["show_hidden_initial"] = True
+        else:
+            defaults["initial"] = self.get_default()
+    if self.choices is not None:
+        # Fields with choices get special treatment.
+        include_blank = self.blank or not (
+                self.has_default() or "initial" in kwargs
+        )
+        defaults["choices"] = self.get_choices(include_blank=include_blank)
+        defaults["coerce"] = self.to_python
+        if self.null:
+            defaults["empty_value"] = None
+        if choices_form_class is not None:
+            form_class = choices_form_class
+        else:
+            form_class = forms.TypedChoiceField
+        # Many of the subclass-specific formfield arguments (min_value,
+        # max_value) don't apply for choice fields, so be sure to only pass
+        # the values that TypedChoiceField will understand.
+        for k in list(kwargs):
+            if k not in (
+                    "coerce",
+                    "empty_value",
+                    "choices",
+                    "required",
+                    "widget",
+                    "label",
+                    "initial",
+                    "help_text",
+                    "error_messages",
+                    "show_hidden_initial",
+                    "disabled",
+            ):
+                del kwargs[k]
+    defaults.update(kwargs)
+    if form_class is None:
+        form_class = forms.CharField
+    return form_class(**defaults)
+
+
+
 Field.__init__ = __init__
+Field.formfield = formfield
 Field.non_db_attrs += ('group',)  # Не отражается на базе, соответственно — не требует и не триггерит миграции
