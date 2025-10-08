5eefab3fa47d209b333691bf6b96351870f213ef
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-10-08 11:51:32 +0300

    Yet another commit

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class DaosUser(models.Model):
-    failed_login_attempts = models.IntegerField(default=0)
-    last_failed_login_attempt_at = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        abstract = True
